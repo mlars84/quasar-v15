63ce95c153dafa98ea45433e9b20d51100c6546e
Matt Larson <matt.a.larson@gmail.com>
2018-02-24 18:30:05 -0600

    init commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>Hello World Quasar v0.15.2!</div>
+</template>
+
+<script>
+import { openURL } from 'quasar'
+
+export default {
+  name: 'LayoutDefault',
+  data () {
+    return {
+      leftDrawerOpen: false
+    }
+  },
+  methods: {
+    openURL
+  }
+}
+</script>
+
+<style>
+</style>
